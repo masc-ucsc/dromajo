4b97add1024060b1e9d335a5d76af85276dbad02
Jose Renau <renau@ucsc.edu>
2022-10-31 20:51:36 -0700

    get the code more portable

--- include/cutils.h
+++ include/cutils.h
@@ -98,10 +98,10 @@ static inline int min_int(int a, int b) {
 
 void *mallocz(size_t size);
 
-#if defined(__APPLE__)
 static inline uint32_t bswap_32(uint32_t v) {
     return ((v & 0xff000000) >> 24) | ((v & 0x00ff0000) >> 8) | ((v & 0x0000ff00) << 8) | ((v & 0x000000ff) << 24);
 }
+#if defined(__APPLE__)
 #include <sys/select.h>
 #else
 #include <byteswap.h>
